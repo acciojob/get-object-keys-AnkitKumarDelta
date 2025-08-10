8d7403806cc722354d90671c6f8c80d3f4d4b9a5
Ankit <136968673+AnkitKumarDelta@users.noreply.github.com>
2025-08-10 06:52:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //your JS code here. If required.
 const student = {
 	name:"ankit",
+	getKeys(obj){
+		return Object.keys(obj);
+	}
 };
 
-function getKeys(obj){
-	return Object.keys(obj);
-}
 
-getKeys(student);
+
+student.getKeys(student);
